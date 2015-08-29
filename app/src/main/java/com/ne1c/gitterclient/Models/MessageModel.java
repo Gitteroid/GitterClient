fac5e1a8e3bf8d916ef4e9a95ccc956f750b7bce
Ne1c <codeneic@gmail.com>
2015-08-29 23:44:17 +0300

    Implemented parcelable.

--- app/src/main/java/com/ne1c/gitterclient/Models/MessageModel.java
+++ app/src/main/java/com/ne1c/gitterclient/Models/MessageModel.java
@@ -1,11 +1,13 @@
 package com.ne1c.gitterclient.Models;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class MessageModel {
+public class MessageModel implements Parcelable {
     public String id;
     public String text;
     public String html;
@@ -23,4 +25,50 @@ public class MessageModel {
         public String screenName;
         public String userId;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.text);
+        dest.writeString(this.html);
+        dest.writeString(this.sent);
+        dest.writeString(this.editedAt);
+        dest.writeParcelable(this.fromUser, flags);
+        dest.writeByte(unread ? (byte) 1 : (byte) 0);
+        dest.writeInt(this.readBy);
+        dest.writeList(this.mentions);
+        dest.writeInt(this.v);
+    }
+
+    public MessageModel() {
+    }
+
+    protected MessageModel(Parcel in) {
+        this.id = in.readString();
+        this.text = in.readString();
+        this.html = in.readString();
+        this.sent = in.readString();
+        this.editedAt = in.readString();
+        this.fromUser = in.readParcelable(UserModel.class.getClassLoader());
+        this.unread = in.readByte() != 0;
+        this.readBy = in.readInt();
+        this.mentions = new ArrayList<Mentions>();
+        in.readList(this.mentions, List.class.getClassLoader());
+        this.v = in.readInt();
+    }
+
+    public static final Parcelable.Creator<MessageModel> CREATOR = new Parcelable.Creator<MessageModel>() {
+        public MessageModel createFromParcel(Parcel source) {
+            return new MessageModel(source);
+        }
+
+        public MessageModel[] newArray(int size) {
+            return new MessageModel[size];
+        }
+    };
 }
--- app/src/main/java/com/ne1c/gitterclient/Models/RoomModel.java
+++ app/src/main/java/com/ne1c/gitterclient/Models/RoomModel.java
@@ -1,8 +1,11 @@
 package com.ne1c.gitterclient.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class RoomModel {
+public class RoomModel implements Parcelable {
     public String id;
     public String name;
     public String topic;
@@ -16,4 +19,57 @@ public class RoomModel {
     public String url;
     public String githubType;
     public int v;
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.topic);
+        dest.writeByte(oneToOne ? (byte) 1 : (byte) 0);
+        dest.writeList(this.users);
+        dest.writeInt(this.unreadItems);
+        dest.writeInt(this.userCount);
+        dest.writeInt(this.mentions);
+        dest.writeString(this.lastAccessTime);
+        dest.writeByte(lurk ? (byte) 1 : (byte) 0);
+        dest.writeString(this.url);
+        dest.writeString(this.githubType);
+        dest.writeInt(this.v);
+    }
+
+    public RoomModel() {
+    }
+
+    protected RoomModel(Parcel in) {
+        this.id = in.readString();
+        this.name = in.readString();
+        this.topic = in.readString();
+        this.oneToOne = in.readByte() != 0;
+        this.users = new ArrayList<UserModel>();
+        in.readList(this.users, ArrayList.class.getClassLoader());
+        this.unreadItems = in.readInt();
+        this.userCount = in.readInt();
+        this.mentions = in.readInt();
+        this.lastAccessTime = in.readString();
+        this.lurk = in.readByte() != 0;
+        this.url = in.readString();
+        this.githubType = in.readString();
+        this.v = in.readInt();
+    }
+
+    public static final Parcelable.Creator<RoomModel> CREATOR = new Parcelable.Creator<RoomModel>() {
+        public RoomModel createFromParcel(Parcel source) {
+            return new RoomModel(source);
+        }
+
+        public RoomModel[] newArray(int size) {
+            return new RoomModel[size];
+        }
+    };
 }
