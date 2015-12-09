abaa6ae934de2dc4461b36a10a5ef258de19679e
ne1c <codeneic@gmail.com>
2015-12-10 00:50:27 +0200

    Refactoring service

--- app/src/main/java/com/ne1c/developerstalk/services/NewMessagesService.java
+++ app/src/main/java/com/ne1c/developerstalk/services/NewMessagesService.java
@@ -12,8 +12,8 @@ import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -21,33 +21,31 @@ import android.text.style.StyleSpan;
 import android.util.Log;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.ne1c.developerstalk.ui.activities.MainActivity;
-import com.ne1c.developerstalk.models.MessageModel;
-import com.ne1c.developerstalk.models.RoomModel;
 import com.ne1c.developerstalk.R;
 import com.ne1c.developerstalk.api.GitterApi;
+import com.ne1c.developerstalk.database.ClientDatabase;
+import com.ne1c.developerstalk.models.MessageModel;
+import com.ne1c.developerstalk.models.RoomModel;
+import com.ne1c.developerstalk.ui.activities.MainActivity;
 import com.ne1c.developerstalk.utils.Utils;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.OkClient;
-import retrofit.client.Response;
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
-import rx.functions.Func1;
+import rx.Subscription;
 import rx.observables.AbstractOnSubscribe;
+import rx.subscriptions.CompositeSubscription;
 
-public class NewMessagesService extends Service implements FayeClient.UnexpectedSituationCallback {
+public class NewMessagesService extends Service {
     public static final int NOTIF_REQUEST_CODE = 1000;
     public static final int NOTIF_CODE = 101;
 
@@ -57,13 +55,16 @@ public class NewMessagesService extends Service implements FayeClient.Unexpected
     public static final String TO_ROOM_MESSAGE_EXTRA_KEY = "toRoom";
     public static final String SEND_MESSAGE_EXTRA_KEY = "sendMessageToRoom";
 
-    private FayeClient mFayeClient;
-    private ArrayList<RoomModel> mRoomsList;
+    private List<RoomModel> mRoomsList;
+    private CompositeSubscription mMessagesSubscrptions;
 
     private GitterApi mApiMethods;
 
+    private boolean mEnableNotif;
     private boolean mSound;
     private boolean mVibro;
+    // Get messages only with user name
+    private boolean mWithUserName;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -81,77 +82,64 @@ public class NewMessagesService extends Service implements FayeClient.Unexpected
         IntentFilter filterSendMessage = new IntentFilter();
         filterSendMessage.addAction(BROADCAST_SEND_MESSAGE);
         registerReceiver(sendMessageReceiver, filterSendMessage);
-//
-//        OkHttpClient okHttpClient = new OkHttpClient();
-  //      okHttpClient.setReadTimeout(45, TimeUnit.SECONDS);
-//        okHttpClient.setFollowSslRedirects(true);
-//        okHttpClient.setRetryOnConnectionFailure(true);
-//        okHttpClient.setProtocols(Arrays.asList(Protocol.HTTP_1_1));
-//
+
         OkHttpClient client = new OkHttpClient();
-        client.setReadTimeout(20, TimeUnit.DAYS);
+        client.setReadTimeout(7, TimeUnit.DAYS);
+        client.setConnectTimeout(7, TimeUnit.DAYS);
 
         RestAdapter adapter = new RestAdapter.Builder()
                 .setClient(new OkClient(client))
                 .setEndpoint("https://stream.gitter.im")
                 .build();
         mApiMethods = adapter.create(GitterApi.class);
-        mFayeClient = new FayeClient(this);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        mEnableNotif = prefs.getBoolean("enable_notif", true);
         mSound = prefs.getBoolean("notif_sound", true);
         mVibro = prefs.getBoolean("notif_vibro", true);
+        mWithUserName = prefs.getBoolean("notif_username", false);
 
-        // Reconnect if service start retry and update rooms
-        if (flags == START_FLAG_RETRY) {
-            disconnect();
-        }
+        mRoomsList = new ClientDatabase(getApplicationContext()).getRooms();
+        createSubscribers();
 
         return START_STICKY;
     }
 
     private void createSubscribers() {
-        String channelEndpoint = "/api/v1/rooms/%s/chatMessages";
+        mMessagesSubscrptions = new CompositeSubscription();
 
         for (final RoomModel room : mRoomsList) {
-            mFayeClient.subscribeChannel(String.format(channelEndpoint, room.id))
-                    .subscribe(new Action1<JsonObject>() {
-                        @Override
-                        public void call(JsonObject response) {
-                            Intent intent = new Intent();
-                            intent.setAction(MainActivity.BROADCAST_NEW_MESSAGE);
-                            intent.putExtra(FROM_ROOM_EXTRA_KEY, room);
+            Subscription sub = getMessageStream(room.id).subscribe(message -> {
+                Intent intent = new Intent();
+                intent.setAction(MainActivity.BROADCAST_NEW_MESSAGE);
+                intent.putExtra(FROM_ROOM_EXTRA_KEY, room);
 
-                            Gson gson = new GsonBuilder().create();
-                            MessageModel message = new MessageModel();
-                            if (response.getAsJsonObject("data") != null) {
-                                message = gson.fromJson(
-                                        response.getAsJsonObject("data").getAsJsonObject("model"),
-                                        MessageModel.class);
-                            }
+                intent.putExtra(NEW_MESSAGE_EXTRA_KEY, message);
+                if (message.text != null) {
+                    sendBroadcast(intent);
 
-                            intent.putExtra(NEW_MESSAGE_EXTRA_KEY, message);
-                            if (message.text != null) {
-                                sendBroadcast(intent);
+                    if (mEnableNotif && !message.fromUser.id.equals(Utils.getInstance().getUserPref().id)) {
+                        final String username = Utils.getInstance().getUserPref().username;
 
-                                boolean enableNotif = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
-                                        .getBoolean("enable_notif", true);
-                                if (!message.fromUser.id.equals(Utils.getInstance().getUserPref().id) &&
-                                        enableNotif) {
-                                    sendNotificationMessage(room, message);
-                                }
-                            }
+                        if (mWithUserName && message.text.contains(username)) {
+                            sendNotificationMessage(room, message);
+                        } else if (!mWithUserName) {
+                            sendNotificationMessage(room, message);
                         }
-                    });
+                    }
+                }
+            });
+
+            mMessagesSubscrptions.add(sub);
         }
     }
 
     @Override
     public void onDestroy() {
-        mFayeClient.disconnect();
+        mMessagesSubscrptions.unsubscribe();
         unregisterReceiver(networkChangeReceiver);
         unregisterReceiver(sendMessageReceiver);
         super.onDestroy();
@@ -188,42 +176,11 @@ public class NewMessagesService extends Service implements FayeClient.Unexpected
         @Override
         public void onReceive(Context context, Intent intent) {
             if (Utils.getInstance().isNetworkConnected()) {
-                getMessageStream("55e448fa0fc9f982beaf298c").subscribe(new Action1<MessageModel>() {
-                    @Override
-                    public void call(MessageModel messageModel) {
-                        Log.d("MESSAGE", messageModel.text);
-                        //Toast.makeText(getApplicationContext(), messageModel.text, Toast.LENGTH_SHORT).show();
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        Log.d("ERROR", throwable.getMessage());
-                    }
-                });
-//                try {
-//                    mFayeClient.connect(Utils.GITTER_FAYE_URL, Utils.getInstance().getAccessToken());
-//                    mFayeClient.accessClientIdSubscriber().subscribe(new Action1<Boolean>() {
-//                        @Override
-//                        public void call(Boolean aBoolean) {
-//                            try {
-//                                mRoomsList = mApiMethods.getCurrentUserRooms(Utils.getInstance().getBearer());
-//                                createSubscribers();
-//                            } catch (RetrofitError e) {
-//                                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-//                            }
-//
-//                        }
-//                    });
-//                } catch (OnErrorNotImplementedException | RetrofitError e) {
-//                    if (e.getMessage().contains("401")) {
-//                            sendBroadcast(new Intent(MainActivity.BROADCAST_UNAUTHORIZED));
-//                        }
-//                    disconnect();
-//                }
-            } else {
-                if (mFayeClient != null) {
-                    mFayeClient.disconnect();
+                if (mMessagesSubscrptions.isUnsubscribed()) {
+                    createSubscribers();
                 }
+            } else {
+                mMessagesSubscrptions.unsubscribe();
             }
         }
     };
@@ -271,44 +228,20 @@ public class NewMessagesService extends Service implements FayeClient.Unexpected
         notifMgr.notify(NOTIF_CODE, notification);
     }
 
-    @Override
-    public void disconnect() {
-//        mFayeClient.reconnect(Utils.getInstance().getAccessToken(), new Action1<Boolean>() {
-//            @Override
-//            public void call(Boolean aBoolean) {
-//                mRoomsList = mApiMethods.getCurrentUserRooms(Utils.getInstance().getBearer());
-//                createSubscribers();
-//            }
-//        });
-    }
-
     private Observable<MessageModel> getMessageStream(String roomId) {
         return mApiMethods.getRoomStream(Utils.getInstance().getBearer(), roomId)
-                .flatMap(new Func1<Response, Observable<String>>() {
-                    @Override
-                    public Observable<String> call(Response response) {
-                        try {
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody().in()));
-                            return Observable.create(new OnSubscribeBufferedReader(bufferedReader));
-                        } catch (IOException e) {
-                            return Observable.error(e);
-                        }
+                .flatMap(response -> {
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody().in()));
+                        return Observable.create(new OnSubscribeBufferedReader(bufferedReader));
+                    } catch (IOException e) {
+                        return Observable.error(e);
                     }
-                }).filter(new Func1<String, Boolean>() {
-                    @Override
-                    public Boolean call(String s) {
-                        return s != null && !s.trim().isEmpty();
-                    }
-                }).map(new Func1<String, MessageModel>() {
-                    @Override
-                    public MessageModel call(String s) {
-                        return new Gson().fromJson(s, MessageModel.class);
-                    }
-                });
+                }).filter(s -> s != null && !s.trim().isEmpty())
+                .map(s -> new Gson().fromJson(s, MessageModel.class));
     }
 
     public class OnSubscribeBufferedReader extends AbstractOnSubscribe<String, BufferedReader> {
-
         private final BufferedReader reader;
 
         public OnSubscribeBufferedReader(BufferedReader reader) {
@@ -343,10 +276,9 @@ public class NewMessagesService extends Service implements FayeClient.Unexpected
                 try {
                     state.close();
                 } catch (IOException e) {
-//        Ignore this exception
+                // Ignore this exception
                 }
             }
         }
     }
 }
-
