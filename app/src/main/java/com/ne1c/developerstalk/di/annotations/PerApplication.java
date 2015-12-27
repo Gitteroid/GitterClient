8679c23b26b2ea692ae8ed3f61ab54dd61aa5601
ne1c <codeneic@gmail.com>
2015-12-27 13:30:41 +0200

    Dagger scopes

--- app/src/main/java/com/ne1c/developerstalk/di/annotations/PerApplication.java
+++ app/src/main/java/com/ne1c/developerstalk/di/annotations/PerApplication.java
@@ -7,5 +7,5 @@ import javax.inject.Scope;
 
 @Scope
 @Retention(RetentionPolicy.RUNTIME)
-public @interface PerActivity {
+public @interface PerApplication {
 }
--- app/src/main/java/com/ne1c/developerstalk/di/annotations/PerFragment.java
+++ app/src/main/java/com/ne1c/developerstalk/di/annotations/PerFragment.java
@@ -1,12 +1,10 @@
 package com.ne1c.developerstalk.di.annotations;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 import javax.inject.Scope;
 
-@Documented
 @Scope
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PerFragment {
