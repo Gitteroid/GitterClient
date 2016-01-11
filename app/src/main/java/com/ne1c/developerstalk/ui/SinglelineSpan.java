361f7ab37dd093257b48506364e83cc23da79a7c
ne1c <codeneic@gmail.com>
2016-01-11 10:53:23 +0200

    Fix border

--- app/src/main/java/com/ne1c/developerstalk/ui/SinglelineSpan.java
+++ app/src/main/java/com/ne1c/developerstalk/ui/SinglelineSpan.java
@@ -14,15 +14,17 @@ public class SinglelineSpan extends ReplacementSpan {
 
     @Override
     public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-        RectF rect = new RectF(x + 2, top + 2, x + measureText(paint, text, start, end) - 2, bottom - 2); // Padding for border
-        RectF rectCorners = new RectF(x, top, x + measureText(paint, text, start, end), bottom);
+        RectF rect = new RectF(x, top, x + measureText(paint, text, start, end), bottom);
 
-        paint.setColor(Color.rgb(185, 185, 185));
-        canvas.drawRoundRect(rectCorners, 5, 5, paint);
-
+        paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.rgb(235, 235, 235));
-        canvas.drawRect(rect, paint);
+        canvas.drawRoundRect(rect, 4, 4, paint);
 
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.rgb(185, 185, 185));
+        canvas.drawRoundRect(rect, 4, 4, paint);
+
+        paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.BLACK);
         canvas.drawText(text, start, end, x, y, paint);
     }
