3d2167b9a5a011d85ad332bcb45f9121117a99f7
Nikolay <codeneic@gmail.com>
2016-05-31 22:37:25 +0300

    Fix title and removing page

--- app/src/main/java/com/ne1c/gitteroid/ui/adapters/RoomsPagerAdapter.java
+++ app/src/main/java/com/ne1c/gitteroid/ui/adapters/RoomsPagerAdapter.java
@@ -11,15 +11,25 @@ import java.util.ArrayList;
 
 public class RoomsPagerAdapter extends FragmentPagerAdapter {
     private ArrayList<RoomViewModel> mRoomsList;
+
     public RoomsPagerAdapter(FragmentManager fm, ArrayList<RoomViewModel> roomsList) {
         super(fm);
-
         mRoomsList = roomsList;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return ChatRoomFragment.newInstance(mRoomsList.get(position));
+        return ChatRoomFragment.newInstance(mRoomsList.get(position), false);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mRoomsList.get(position).name;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
     }
 
     @Override
