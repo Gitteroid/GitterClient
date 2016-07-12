5b40a16a387a03650448d9627e6ec17242bf921e
n.kucheriaviy <n.kucheriaviy@smartweb.com.ua>
2016-07-12 17:36:32 +0300

    Update UI

--- app/src/main/java/com/ne1c/gitteroid/ui/activities/SearchRoomActivity.java
+++ app/src/main/java/com/ne1c/gitteroid/ui/activities/SearchRoomActivity.java
@@ -1,9 +1,11 @@
 package com.ne1c.gitteroid.ui.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.ne1c.gitteroid.R;
 import com.ne1c.gitteroid.di.components.DaggerSearchRoomComponent;
@@ -19,6 +21,8 @@ public class SearchRoomActivity extends BaseActivity {
     private SearchRoomComponent mComponent;
 
     private EditText mSearchEditText;
+    private LinearLayout mNoResultLayout;
+    private RecyclerView mResultRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class SearchRoomActivity extends BaseActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mSearchEditText = (EditText) findViewById(R.id.search_editText);
+        mNoResultLayout = (LinearLayout) findViewById(R.id.no_result_layout);
 
         findViewById(R.id.clear_imageButton).setOnClickListener(v -> mSearchEditText.getText().clear());
     }
